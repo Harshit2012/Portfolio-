dd8fc159fd7a7fef574677c2409def11af86450e
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2024-04-22 12:18:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function viewg(){
+    window.location = "https://github.com/Harshit2012";
+}
+
+function openmw(){
+    window.location = "https://github.com/Harshit2012/Messa_World"
+}
+
+function openip(){
+    window.location = "https://github.com/Harshit2012/InkPad"
+}
